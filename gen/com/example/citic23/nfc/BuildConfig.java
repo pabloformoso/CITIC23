06474ab4313d4e49303b83d8c290e2d6b07f5563
Pablo Formoso Estrada <pablo@pabloformoso.com>
2013-05-03 01:14:55 +0200

    first

---
+++ gen/com/example/citic23/nfc/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.citic23.nfc;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/citic23/nfc/SendActivity.java
@@ -0,0 +1,40 @@
+package com.example.citic23.nfc;
+
+import android.app.Activity;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class SendActivity extends Activity {
+
+    private NfcAdapter mAdapter;
+    private TextView mText;
+    private NdefMessage mMessage;
+
+    @Override
+    public void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+        setContentView(R.layout.activity_send);
+        mText = (TextView) findViewById(R.id.textView1);
+        
+        mAdapter = NfcAdapter.getDefaultAdapter(this);
+        mMessage = new NdefMessage(NdefRecord.createUri("http://www.android.com"));
+
+        if (mAdapter != null) {
+            mAdapter.setNdefPushMessage(mMessage, this);
+            mText.setText("Enviado TAG con las url android.com");
+        } else {
+            mText.setText("Tu tlf no dispone de NFC");
+        }
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.send, menu);
+		return true;
+	}
+
+}
